b92861774a836dc2512bdc60c094255013a29c01
Steven Skoczen <steven.skoczen@wk.com>
2012-01-13 15:31:53 -0800

    updated license to 3-clause BSD, PEP-8ified setup

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
 setup(
-    name = "salad",
-    description = "A nice mix of great BDD ingredients",
-    author = "Steven Skoczen",
-    author_email = "steven.skoczen@wk.com",
-    url = "https://github.com/wieden-kennedy/salad",
-    version = "0.1",
+    name="salad",
+    description="A nice mix of great BDD ingredients",
+    author="Steven Skoczen",
+    author_email="steven.skoczen@wk.com",
+    url="https://github.com/wieden-kennedy/salad",
+    version="0.1",
     requires=["lettuce", "nose", "splinter"],
-    packages = find_packages(),
-    zip_safe = False,
+    packages=find_packages(),
+    zip_safe=False,
     include_package_data=True,
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",

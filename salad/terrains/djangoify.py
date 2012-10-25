f683c77f8ee8b7dd205eeeb8fcc56ecaed286fbe
Steven Skoczen <steven.skoczen@buildingenergy.com>
2012-10-25 15:09:02 -0700

    Bugfix of db setup/teardown, version bump

--- salad/terrains/djangoify.py
+++ salad/terrains/djangoify.py
@@ -1,24 +1,21 @@
 from lettuce import before, world, after
 from salad.logger import logger
 
-old_database_name = None
-
 try:
-    logger = getLogger(__name__)
     logger.info("Loading the terrain file...")
 
-    from django.core.management import call_command
-    from django.conf import settings
-    from django.test.simple import DjangoTestSuiteRunner
-    from django.core import mail
-    try:
-        from south.management.commands import patch_for_test_db_setup
-        USE_SOUTH = getattr(settings, "SOUTH_TESTS_MIGRATE", False)
-    except:
-        USE_SOUTH = False
+    @before.all
+    def setup_database():
+        from django.core.management import call_command
+        from django.conf import settings
+        from django.test.simple import DjangoTestSuiteRunner
 
-    @before.runserver
-    def setup_database(actual_server):
+        try:
+            from south.management.commands import patch_for_test_db_setup
+            USE_SOUTH = getattr(settings, "SOUTH_TESTS_MIGRATE", False)
+        except:
+            USE_SOUTH = False
+
         logger.info("Setting up a test database...")
 
         if USE_SOUTH:
@@ -29,11 +26,13 @@ try:
         world.created_db = DjangoTestSuiteRunner.setup_databases(world.test_runner)
 
         call_command('syncdb', interactive=False, verbosity=0)
-        call_command('migrate', interactive=False, verbosity=0)
+        if USE_SOUTH:
+            call_command('migrate', interactive=False, verbosity=0)
         call_command('loaddata', 'all', verbosity=0)
 
-    @after.runserver
+    @after.all
     def teardown_database(actual_server):
+        from django.test.simple import DjangoTestSuiteRunner
         logger.info("Destroying the test database ...")
 
         DjangoTestSuiteRunner.teardown_databases(world.test_runner, world.created_db)
@@ -41,6 +40,8 @@ try:
 
     @before.each_scenario
     def reset_data(scenario):
+        from django.core.management import call_command
+
         # Clean up django.
         logger.info("Flushing the test database...")
         call_command('flush', interactive=False, verbosity=0)
@@ -48,13 +49,16 @@ try:
 
     @before.each_feature
     def empty_outbox(scenario):
+        from django.core import mail
         logger.info("Emptying outbox...")
         mail.outbox = []
 
 except:
+    from traceback import print_exc
+    print_exc()
     try:
         # Only complain if it seems likely that using django was intended.
         import django
-        logger.warn("Django terrains not imported.")
+        logger.info("Django terrains not imported.")
     except:
         pass

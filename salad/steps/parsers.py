9673bb10084c8481e96227119f19752e0a065755
Jana Rekittke <jana.rekittke@deliveryhero.com>
2013-03-07 15:28:47 +0100

    removed comment and added some error message

--- salad/steps/parsers.py
+++ salad/steps/parsers.py
@@ -6,7 +6,7 @@ def pick_to_index(pick_string):
     elif pick == "last":
         return -1
     try:
-        return int(pick.strip('st nd rd th')) - 1  # strip 'th' or 'rd off
+        return int(pick.strip('st nd rd th')) - 1
     except ValueError:
         raise ValueError("Could not convert '%s'" % pick +
-                         " Only supports 'first', 'last', and '##nd'")
+                " This supports: 'first', 'last', '1st', '2nd', '3rd', '4th', '5th',..")

143b065d83fb321174e2b4c6455df4c6c1f65c1f
Jana Rekittke <jana.rekittke@deliveryhero.com>
2013-03-07 10:00:21 +0100

    added TimeoutException and removed message parameter in until. A message is now automatically added to the TimeoutException

--- salad/waiter.py
+++ salad/waiter.py
@@ -1,52 +1,27 @@
-#!/usr/bin/python
-#
-# Copyright 2011 Software Freedom Conservancy.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import time
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
-from salad.logger import logger
 
 POLL_FREQUENCY = 0.5  # How long to sleep inbetween calls to the method
-IGNORED_EXCEPTIONS = [NoSuchElementException] # list of exceptions ignored during calls to the method
 
 class SaladWaiter(object):
 
-    def __init__(self, driver, timeout, poll_frequency=POLL_FREQUENCY, ignored_exceptions=None):
-        """Constructor, takes a WebDriver instance and timeout in seconds.
-           :Args:
-            - driver - Instance of WebDriver (Ie, Firefox, Chrome or Remote)
+    def __init__(self, timeout, poll_frequency=POLL_FREQUENCY, ignored_exceptions=None):
+        """Constructor
+           Args:
             - timeout - Number of seconds before timing out
             - poll_frequency - sleep interval between calls
               By default, it is 0.5 second.
             - ignored_exceptions - iterable structure of exception classes ignored during calls.
-              By default, it contains NoSuchElementException only.
 
            Example:
-            from selenium.webdriver.support.ui import WebDriverWait
-            element = WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id("someId"))
-            is_disappeared = WebDriverWait(driver, 30, 1, (ElementNotVisibleException)).\
-                        until_not(lambda x: x.find_element_by_id("someId").is_displayed())
+            from salad.waiter import SaladWaiter
+            element = SaladWaiter(10).until(False, some_method, method_argument1, method_argument2,..)
         """
-        self._driver = driver
         self._timeout = timeout
         self._poll = poll_frequency
         # avoid the divide by zero
         if self._poll == 0:
             self._poll = POLL_FREQUENCY
-        exceptions = IGNORED_EXCEPTIONS
+        exceptions = []
         if ignored_exceptions is not None:
             try:
                 exceptions.extend(iter(ignored_exceptions))
@@ -54,10 +29,10 @@ class SaladWaiter(object):
                 exceptions.append(ignored_exceptions)
         self._ignored_exceptions = tuple(exceptions)
 
-    def until(self, message, negate, method, *args):
-        """Calls the method provided with the driver as an argument until the \
-        return value is not False or in case negate is True until the return \
-        value is False."""
+    def until(self, negate, method, *args):
+        """The provided method should return either True or False.
+        It is then called until proper return value appears according to negate
+        OR until timeout happens"""
         end_time = time.time() + self._timeout
         while(True):
             try:
@@ -73,4 +48,8 @@ class SaladWaiter(object):
             time.sleep(self._poll)
             if(time.time() > end_time):
                 break
-        raise TimeoutException(message)
+        raise TimeoutException("TimeoutException in method %s" % method.func_name)
+
+
+class TimeoutException(Exception):
+    pass

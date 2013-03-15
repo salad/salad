59b265bf3f439ca6d7ae99267c975a2f35505602
Jana Rekittke <jana.rekittke@deliveryhero.com>
2013-03-15 14:03:10 +0100

    added support for specific pick to selecting. works only partly. got rid off _convert_pattern_to_css

--- salad/steps/browser/forms.py
+++ salad/steps/browser/forms.py
@@ -1,9 +1,10 @@
 from time import sleep
+from selenium.webdriver.support.ui import Select
 from lettuce import step, world
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.errorhandler import StaleElementReferenceException
 from salad.steps.browser.finders import (PICK_EXPRESSION, ELEMENT_FINDERS, ELEMENT_THING_STRING,
-        _get_visible_element, _convert_pattern_to_css)
+        _get_visible_element)
 from salad.tests.util import assert_equals_with_negate
 
 # What's happening here? We're generating steps for every possible permuation of the element finder
@@ -26,20 +27,20 @@ for finder_string, finder_function in ELEMENT_FINDERS.iteritems():
 
 
     def _select_generator(finder_string, finder_function):
-	@step(r'select the option (named|with the value)? "([^"]*)" (?:from|in) the( first)?( last)? %s %s' % (
-                ELEMENT_THING_STRING, finder_string))
-	def _this_step(step, named_or_with_value, field_value, first, last, find_pattern):
-		css = _convert_pattern_to_css(finder_function, first, last, find_pattern, tag="select")
+        @step(r'select the option (named|with the value)? "([^"]*)" (?:from|in) the%s %s %s' % (PICK_EXPRESSION, ELEMENT_THING_STRING, finder_string))
+        def _this_step(step, named_or_with_value, field_value, pick, find_pattern):
+            ele = _get_visible_element(finder_function, pick, find_pattern)
+            if (named_or_with_value == "named"):
+                # this does not work properly, it will click the first match
+                # to field_value by default. it does not select the element we
+                # are actually looking for.
+                option = world.browser.find_option_by_text(field_value)
+            else:
+                option = ele.find_by_value(field_value)
 
-		if named_or_with_value == "with the value":
-			css += " option[value='%s']" % (field_value,)
-			ele = world.browser.find_by_css(css).first
-		else:
-			ele = world.browser.find_option_by_text(field_value)
+            option.click()
 
-		ele.click()
-
-	return _this_step
+        return _this_step
 
     globals()["form_select_%s" % (finder_function,)] = _select_generator(finder_string, finder_function)
 

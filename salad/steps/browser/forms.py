ce7f65111a133eb7842c89b2dfa6e186ecb53d43
Jana Rekittke <jana.rekittke@deliveryhero.com>
2013-03-15 09:34:51 +0100

    changed type_generator to work without convert_pattern_to_css

--- salad/steps/browser/forms.py
+++ salad/steps/browser/forms.py
@@ -1,13 +1,47 @@
+from time import sleep
 from lettuce import step, world
-from splinter.driver.webdriver import TypeIterator
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.errorhandler import StaleElementReferenceException
-from salad.steps.browser.finders import ELEMENT_FINDERS, ELEMENT_THING_STRING, _get_visible_element, _convert_pattern_to_css
+from salad.steps.browser.finders import (PICK_EXPRESSION, ELEMENT_FINDERS, ELEMENT_THING_STRING,
+        _get_visible_element, _convert_pattern_to_css)
 from salad.tests.util import assert_equals_with_negate
 
 # What's happening here? We're generating steps for every possible permuation of the element finder
 
 for finder_string, finder_function in ELEMENT_FINDERS.iteritems():
+
+    def _type_generator(finder_string, finder_function):
+        @step(r'(slowly )?type "([^"]*)" into the%s %s %s' % (
+                PICK_EXPRESSION, ELEMENT_THING_STRING, finder_string))
+        def _this_step(step, slowly, text, pick, find_pattern):
+            ele = _get_visible_element(finder_function, pick, find_pattern)
+            if slowly and slowly != "":
+                _type_slowly(ele, text)
+            else:
+                ele.value = text
+
+        return _this_step
+
+    globals()["form_type_%s" % (finder_function,)] = _type_generator(finder_string, finder_function)
+
+
+    def _select_generator(finder_string, finder_function):
+	@step(r'select the option (named|with the value)? "([^"]*)" (?:from|in) the( first)?( last)? %s %s' % (
+                ELEMENT_THING_STRING, finder_string))
+	def _this_step(step, named_or_with_value, field_value, first, last, find_pattern):
+		css = _convert_pattern_to_css(finder_function, first, last, find_pattern, tag="select")
+
+		if named_or_with_value == "with the value":
+			css += " option[value='%s']" % (field_value,)
+			ele = world.browser.find_by_css(css).first
+		else:
+			ele = world.browser.find_option_by_text(field_value)
+
+		ele.click()
+
+	return _this_step
+
+    globals()["form_select_%s" % (finder_function,)] = _select_generator(finder_string, finder_function)
 
     def _fill_generator(finder_string, finder_function):
         @step(r'fill in the( first| last)? %s %s with "([^"]*)"' % (ELEMENT_THING_STRING, finder_string))

a86787cc040d6bac73e873f3251a065d2eb6c7c0
Jana Rekittke <jana@rekittke.name>
2014-10-22 09:02:48 +0200

    add step to take screenshot and tests for it

--- salad/features/steps-browser-general.feature
+++ salad/features/steps-browser-general.feature
@@ -1,4 +1,4 @@
-Feature: Test the general steps
+Feature: Test the general steps and take screenshot step
 
     Scenario: 1. Test the general steps
         Given I visit the salad test url "browser/elements.html"
@@ -9,7 +9,17 @@ Feature: Test the general steps
          When I remember the text of the element with the id "i_contain" as "my_text"
          Then I print out the stored value of "my_text"
 
+
     Scenario: 2. This test should fail!
         Given I visit the salad test url "browser/elements.html"
          When I look around
          Then I should fail because "This step does not make sense"
+
+
+    Scenario: 3. Take a screenshot
+        Given I visit the salad test url "browser/elements.html"
+         When I look around
+         Then I take a screenshot
+          And I take a screenshot named "my_screenshot"
+          And I take a screenshot with timestamp
+          And I take a screenshot named "bla" with timestamp
--- salad/steps/browser/browsers.py
+++ salad/steps/browser/browsers.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+from time import time
+
 from lettuce import step, world
 from salad.terrains.browser import setup_browser
-
-# Choose which browser to use
+from salad.tests.util import generate_random_string
 
 
 @step(r'am using ([^"]*)$')
@@ -9,3 +11,21 @@ def using_alternative_browser(step, browser_name):
     driver = browser_name.lower().replace(' ', '')
     world.browsers.append(setup_browser(driver))
     world.browser = world.browsers[-1]
+
+
+@step(r'take a screenshot(?: named "([^"]+)")?( with timestamp)?')
+def take_screenshot(step, name, with_timestamp):
+    ts = ''
+
+    if with_timestamp:
+        ts = datetime.fromtimestamp(time()).strftime('%Y-%m-%d_%H:%M:%S')
+        ts = '_%s' % (ts, )
+        if not name:
+            name = 'screenshot'
+
+    if not (with_timestamp or name):
+        suffix = generate_random_string(10)
+        name = 'screenshot_%s' % (suffix, )
+
+    name = '/tmp/%s%s.png' % (name, ts)
+    world.browser.driver.get_screenshot_as_file(name)

a1b80e18a31f2f35b09d04e55870553e01575797
Jana Rekittke <jana@rekittke.name>
2014-11-07 12:39:42 +0100

    let the argparser handle the argument

--- salad/cli.py
+++ salad/cli.py
@@ -52,6 +52,9 @@ def main(args=sys.argv[1:]):
     parser.add_argument('--timeout',
                         help=("Set the saucelabs' idle-timeout for the job"))
 
+    parser.add_argument('--scenarios', '-s',
+                        help=("Limit to the specified scenarios"))
+
     (parsed_args, leftovers) = parser.parse_known_args()
     world.drivers = [parsed_args.browser]
     world.remote_url = parsed_args.remote_url
@@ -78,24 +81,14 @@ def main(args=sys.argv[1:]):
     world.remote_capabilities['trustAllSSLCertificates'] = True
     world.remote_capabilities['acceptSslCerts'] = True
 
-    scenarios = None
-    if leftovers and any('-s' in x for x in leftovers):
-        param, index = [(x, leftovers.index(x)) for x in leftovers
-                        if '-s' in x][0]
-        scenarios = param.replace('-s', '').split(',')
-
-    if scenarios:
-        new_param = set()
-        for item in scenarios:
-            if '-' not in item:
-                new_param.add(int(item))
-            else:
-                start, end = re.search(r'(\d+)-(\d+)', item).groups()
-                numbers = set(range(int(start), int(end)+1))
-                new_param = new_param.union(numbers)
-        new_param = [str(x) for x in new_param]
-        new_param = '-s%s' % (','.join(new_param), )
-        leftovers[index] = new_param
+    if parsed_args.scenarios:
+        scenarios = set()
+        for part in parsed_args.scenarios.split(','):
+            x = part.split('-')
+            scenarios.update(range(int(x[0]),int(x[-1])+1))
+        scenarios = [str(x) for x in sorted(scenarios)]
+        leftovers.append('-s %s' % (','.join(scenarios)))
+        del parsed_args.scenarios
 
     lettuce_main(args=leftovers)
 
